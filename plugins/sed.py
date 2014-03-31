61ac98a892a553aa07ad2d24f3ded0d7daedd391
nathan0 <nathan0@users.noreply.github.com>
2014-03-31 08:54:06 +0100

    Update sed.py

--- plugins/sed.py
+++ plugins/sed.py
@@ -4,8 +4,6 @@ import re, time
 
 @hook.regex('^s\/(.*?)\/(.*?)\/')
 def sed(inp, db=None, say=None):
-    if inp.group(1).startswith("^"): search = inp.group(1)[1:]
-    else: search = inp.group(1)
     m = db.execute("select nick, text from sed where text like (?) order by time desc", ("%"+search+"%",)).fetchone()
     if m:
         nick = m[0]
@@ -15,8 +13,7 @@ def sed(inp, db=None, say=None):
 @hook.singlethread
 @hook.event('PRIVMSG')
 def sed_save(inp,input=None,conn=None,db=None):
-    db.execute("create table if not exists sed(nick PRIMARY KEY, text, time)")
-    nick = input.nick
+    db.execute("CREATE TABLE IF NOT EXISTS sed(nick, text, time, CONSTRAINT primarykey PRIMARY KEY (nick, time))")
     time.sleep(1)
-    db.execute("insert into sed(nick, text, time) values (?,?,?)", (nick, inp[1], int(time.time())))
+    db.execute("insert into sed(nick, text, time) values (?,?,?)", (input.nick, inp[1], time.time()))
     db.commit()
