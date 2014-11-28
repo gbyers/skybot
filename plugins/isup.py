2e611e0671a1040c8b734181d173073997842068
nathan0 <mail@cuddlerape.me>
2014-11-28 06:11:33 +0100

    s/page/text/

--- plugins/isup.py
+++ plugins/isup.py
@@ -5,7 +5,7 @@ def isup(inp):
     "isup <url> -- checks if the website is up or not"
     data = http.get("http://isup.me/%s"%inp)
     if "If you can see this page and still think we're down, it's just you." in data:
-        return ("If you can see this page and still think we're down, it's just you.")
+        return ("If you can see this text and still think we're down, it's just you.")
     elif "It's just you." in data:
         return ("It's just you.")
     else:
